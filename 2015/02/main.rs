f79a0629f30ab92b0af9d05ba79d6a58bccc5b5a
Joona Tiinanen <joona.tiinanen@basemark.com>
2023-09-23 19:17:52 +0300

    2015 01-03

---
+++ 2015/02/main.rs
@@ -0,0 +1,63 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("./2015/02/input.txt");
+
+    // Part 1
+    {
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+
+        let total_area = reader.lines().fold(0, |acc, line| {
+            if let Ok(line) = line {
+                let split: Vec<&str> = line.split("x").collect();
+                let l = split[0].parse::<i32>().unwrap();
+                let w = split[1].parse::<i32>().unwrap();
+                let h = split[2].parse::<i32>().unwrap();
+
+                let a = l * w;
+                let b = w * h;
+                let c = h * l;
+
+                let min = a.min(b).min(c);
+
+                return acc + 2 * a + 2 * b + 2 * c + min;
+            }
+
+            acc
+        });
+
+        println!("P1: {}", total_area);
+    }
+
+    // Part 2
+    {
+        let file = File::open(path).unwrap();
+        let reader = BufReader::new(file);
+
+        let total_length = reader.lines().fold(0, |acc, line| {
+            if let Ok(line) = line {
+                let mut split = line
+                    .split("x")
+                    .map(|dim| dim.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+                split.sort();
+
+                let smallest = split[0];
+                let second_smallest = split[1];
+                let biggest = split[2];
+
+                let ribbon =
+                    smallest * 2 + second_smallest * 2 + smallest * second_smallest * biggest;
+
+                return acc + ribbon;
+            }
+
+            acc
+        });
+
+        println!("P2: {}", total_length);
+    }
+}
---
+++ 2015/03/main.rs
@@ -0,0 +1,59 @@
+use std::collections::HashSet;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("./2015/03/input.txt");
+    let file = fs::read_to_string(path).unwrap();
+
+    // Part 1
+    {
+        let mut map = HashSet::<(i32, i32)>::new();
+        map.insert((0, 0));
+
+        let mut current_coords: (i32, i32) = (0, 0);
+
+        file.chars().for_each(|c| {
+            match c {
+                '>' => current_coords.0 += 1,
+                '<' => current_coords.0 -= 1,
+                '^' => current_coords.1 += 1,
+                'v' => current_coords.1 -= 1,
+                _ => return (),
+            };
+
+            map.insert(current_coords);
+        });
+
+        println!("P1: {}", map.len());
+    }
+
+    // Part 2
+    {
+        let mut map = HashSet::<(i32, i32)>::new();
+        map.insert((0, 0));
+
+        let mut current_coords_santa: (i32, i32) = (0, 0);
+        let mut current_coords_robo: (i32, i32) = (0, 0);
+
+        for (i, c) in file.chars().enumerate() {
+            let current_coords = if i % 2 == 0 {
+                &mut current_coords_santa
+            } else {
+                &mut current_coords_robo
+            };
+
+            match c {
+                '>' => current_coords.0 += 1,
+                '<' => current_coords.0 -= 1,
+                '^' => current_coords.1 += 1,
+                'v' => current_coords.1 -= 1,
+                _ => return (),
+            };
+
+            map.insert(*current_coords);
+        }
+
+        println!("P2: {}", map.len());
+    }
+}
